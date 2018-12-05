fa063e70d212f125142f058bb4f9f4d2751eaa20
Greg Amaroso <greg@Gregs-MBP.home>
2018-12-04 20:57:26 -0500

    .

--- public/button-3.js
+++ public/button-3.js
@@ -1,11 +1,50 @@
+class PGAddToBag extends HTMLButtonElement {
+  constructor() {
+    super();
+    this._skuId = null;
+
+    // this.addEventListener("click", () => {
+    //  alert('here');
+    // });
+  }
+  connectedCallback() {
+  }
+  disconnectedCallback() {
+  }
+}
+
+window.customElements.define(
+  'add-to-bag',
+  PGAddToBag,
+  {
+    extends: "button"
+  }
+);
+
+
+
+
 (function() {
 
+let i = 0;
 const obs = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.type === 'attributes') {
       console.log(mutation);
+
+      const ele = mutation.target;
+      const skuId = ele.dataset.skuId;
+      const button = document.createElement('add-to-bag');
+
+      ele.innerHTML = '';
+      ele.appendChild(button);
     }
   });
+
+  if (++i === 2) {
+    throw new Error("peace out");
+    return false;
+  }
 });
 
 obs.observe(
